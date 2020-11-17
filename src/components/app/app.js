801ff189590049a8953e829a7a0b2db50e17aafa
Roman <outpacroman@gmail.com>
2020-11-17 18:02:38 +0300

    Realises DeleteItem function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,26 +7,35 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: todoData.filter((item) => item.id !== id)
+      };
+    });
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData}
+          onDeleted={this.deleteItem} />
       </div>
-
-      <TodoList todos={todoData}
-        onDeleted={(id) => console.log(`${id} deleted`)} />
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
