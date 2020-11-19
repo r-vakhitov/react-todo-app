f36580504453203103886fb3c3b5554fb0215e1f
Roman <outpacroman@gmail.com>
2020-11-19 17:43:30 +0300

    Add Form

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./add-item.css";
 
-const AddItem = ({ addItem }) => {
-  return (
-    <div className="add-item-form">
-      <button
-        className="btn btn-outline-secondary"
-        type="button"
-        onClick={() => addItem("HEY")}
-      >
-        HEY
-      </button>
-    </div>
-  );
-};
+export default class AddItem extends Component {
 
-export default AddItem;
+  state = {
+    fieldValue: ''
+  }
+
+  onLabelChange = (evt) => {
+    this.setState({
+      fieldValue: evt.target.value,
+    });
+  };
+
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    this.props.addItem(this.state.fieldValue);
+    this.setState({
+      fieldValue: ''
+    });
+  };
+
+  render() {
+
+    return (
+      <form className="add-item-form d-flex"
+        onSubmit={this.onFormSubmit}>
+        <input type="text"
+          className="form-control"
+          placeholder="What need to be done?"
+          onChange={this.onLabelChange}
+          value={this.state.fieldValue} />
+        <button
+          className="btn btn-outline-secondary"
+          type="submit"
+        >
+          Add Item
+      </button>
+      </form>
+    );
+  };
+}
